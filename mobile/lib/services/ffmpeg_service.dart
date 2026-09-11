6d429203b0cd7974110aed94902971c4aa85ca58
sumitsumitdadhich2-spec <sumitsumitdadhich2@gmail.com>
2026-09-11 18:53:28 +0530

    build: migrate to ffmpeg_kit_flutter_new
    Replace ffmpeg_kit_flutter_full with ffmpeg_kit_flutter_new to update FFmpeg dependency.

--- mobile/lib/services/ffmpeg_service.dart
+++ mobile/lib/services/ffmpeg_service.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
-import 'package:ffmpeg_kit_flutter_full/ffmpeg_kit.dart';
-import 'package:ffmpeg_kit_flutter_full/ffmpeg_session.dart';
-import 'package:ffmpeg_kit_flutter_full/return_code.dart';
+import 'package:ffmpeg_kit_flutter_new/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter_new/ffmpeg_session.dart';
+import 'package:ffmpeg_kit_flutter_new/return_code.dart';
 import 'package:path/path.dart' as p;
 import '../utils/constants.dart';
 
